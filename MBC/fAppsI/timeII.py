b6a7622f8790ebb92584ba040f8d9a08b7b2541a
root <root@beaglebone.localdomain>
2018-07-31 20:01:14 -0500

    Making new Changes!

---
+++ MBC/fAppsI/timeII.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+import datetime
+
+@app.route("/")
+def hello():
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+        "time": timeString,
+    }
+    return render_template("mainOne.html", **templateData)
+
+if __name__ == "__main__":
+    app.run(host="192.168.1.18", port=5000, debug=True)
+
--- MBC/fAppsI/timeIII.py
+++ MBC/fAppsI/timeIII.py
@@ -5,11 +5,12 @@ import datetime
 @app.route("/")
 def hello():
     now = datetime.datetime.now()
-    timeString = now.strftime("%Y-%m-%d %H:%M")
+    timeString = now.strftime("%m-%d-%Y %H:%M")
     templateData = {
         "time": timeString,
     }
-    return render_template("main.html", **templateData)
-        
+    return render_template("mainTwo.html", **templateData)
+
 if __name__ == "__main__":
-    app.run(host="192.168.7.2", port=5000, debug=True) 
+    app.run(host="192.168.1.18", port=5000, debug=True)
+
